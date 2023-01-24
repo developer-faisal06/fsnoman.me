bdea10dfc50c26030f5d9cb08a07279193df9ad3
developer-faisal06 <faisal.web.06@gmail.com>
2023-01-25 03:29:41 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 /* -------------------------------------------
 
-Name: 		Arter
-Version:  1.0
-Author:		Nazar Miller (millerDigitalDesign)
-Portfolio:  https://themeforest.net/user/millerdigitaldesign/portfolio?ref=MillerDigitalDesign
 
-p.s. I am available for Freelance hire (UI design, web development). mail: miller.themes@gmail.com
+p.s. I am available for Freelance hire (UI design, web development). faisal.web.06@gmail.com
 
 ------------------------------------------- */
 $(function() {
